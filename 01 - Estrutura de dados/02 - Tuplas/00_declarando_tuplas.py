89ec4c70da12fe468273429dec24199ccb880052
carloscogo84 <carloscogo84@gmail.com>
2024-06-12 09:20:09 +0100

    Tuplas

---
+++ 01 - Estrutura de dados/02 - Tuplas/00_declarando_tuplas.py
@@ -0,0 +1,12 @@
+# Uma boa prática em tupla é adicionar um virgula no final, para o python entender que é uma tupla e não precedencia de operadores
+frutas = ("laranja", "pera", "uva",)
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple([1, 2, 3, 4])
+print(numeros)
+
+pais = ("Brasil,")
+print(pais)
---
+++ 01 - Estrutura de dados/02 - Tuplas/08_len.py
@@ -0,0 +1,13 @@
+linguagens = (
+    "python",
+    "js",
+    "c",
+    "java",
+    "csharp",
+)
+
+print(len(linguagens))  # 5
+
+
+carros = ("gol")
+print(isinstance(carros, tuple))
